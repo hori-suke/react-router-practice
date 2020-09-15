0acf43037f6f8bb957d7f6f33119449939691e50
Yusuke Horie <9kj3hg73@gmail.com>
2020-09-15 22:33:05 +0900

    Fitst commit.

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(props) {
+  const match = props.match;
+  console.log(match);
+  console.log(match.params.id, ': idは?')
+  return (
+      <h1>Aboutです</h1>
+  );
+}
+
+export default About;
